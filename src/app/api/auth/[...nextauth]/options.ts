212ac4d6692cc97f688a0b3ba29505b678c5945b
Ashu Gupta <ashugpt1812@gmail.com>
2024-02-01 16:39:41 +0530

    user data added to db upon signin

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,13 +1,14 @@
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
+import { prisma } from "@/app/providers/PrismaProvider";
 
 export const authOptions : NextAuthOptions  = {
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
             profile(profile) {
-                console.log("Profile GitHub", profile);
+                // console.log("Profile GitHub", profile);
 
                 let userRole = "GitHub User";
                 if(profile?.email == "adobeashu1812@gmail.com") {
@@ -25,7 +26,7 @@ export const authOptions : NextAuthOptions  = {
       // ...add more providers here
         GoogleProvider({
             profile(profile) {
-                console.log("Profile Google", profile);
+                // console.log("Profile Google", profile);
 
                 return {
                     ...profile,
@@ -53,7 +54,29 @@ export const authOptions : NextAuthOptions  = {
                 // session.user.role = token.role;
             }
             return session;
-        }
+        },
+        // Use the signIn() callback to control if a user is allowed to sign in.
+        async signIn({ user }) {
+
+            // Save user data to the database
+            if(user.email && user.name) {
+                await prisma.user.upsert({
+                    where: { email: user.email },
+                    update: { name: user.name },
+                    create: { email: user.email, name: user.name },
+                });
+            }
+            
+            const isAllowedToSignIn = true
+            if (isAllowedToSignIn) {
+              return true
+            } else {
+              // Return false to display a default error message (can use to blocklist some emails)
+              return false
+              // Or you can return a URL to redirect to:
+              // return '/unauthorized'
+            }
+        },
     },
     secret: process.env.NEXTAUTH_SECRET!,
   }
