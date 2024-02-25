b216d02e9d75ca2606f0b0d6116945477174f866
Ashu Gupta <ashugpt1812@gmail.com>
2024-02-26 00:55:54 +0530

    changed card skeletons

--- src/app/uploads/VideoCardSkeleton.tsx
+++ src/app/uploads/VideoCardSkeleton.tsx
@@ -1,25 +1,27 @@
-// import { MoreVertical } from 'lucide-react'
+'use client'
+
+import { MoreVertical } from 'lucide-react'
 import { Skeleton } from "@/components/ui/skeleton"
 
-export default function CardSkeleton() {
+export default function VideoCardSkeleton() {
 
     return (
         
         <div className='flex flex-col gap-1'>
             <Skeleton className="w-full aspect-video md:rounded-xl" />
             <div className='flex w-full justify-between pl-2'>
-                <div><Skeleton className="mt-2 mr-3 w-9 h-9 rounded-full w-10 h-10" /></div>
+                {/* <div><Skeleton className="mt-2 mr-3 w-9 h-9 rounded-full w-10 h-10" /></div> */}
                 
                 <div className='flex flex-col w-full'>
                     <div className='mt-2 text-xs text-gray-500'>
                       <Skeleton className='w-full h-4' />
-                      <Skeleton className='w-full h-3 mt-2' />
+                      <div className='pr-8'><Skeleton className='w-full h-3 mt-2' /></div>
                     </div>
                 </div>
                 
-                {/* <div> */}
-                    {/* <div className='mt-2 ml-3 w-10 h-10 text-center p-2 rounded-full font-bold hover:bg-gray-300'><MoreVertical /></div> */}
-                {/* </div> */}
+                <div>
+                    <div className='mt-2 ml-3 w-10 h-10 text-center p-2 rounded-full font-bold hover:bg-gray-300'><MoreVertical /></div>
+                </div>
             </div>
         </div>
     )
--- src/app/uploads/page.tsx
+++ src/app/uploads/page.tsx
@@ -5,12 +5,13 @@ import { useSearchParams } from 'next/navigation'
 import { Video } from '@prisma/client'
 import { Button } from "@/components/ui/button"
 import VideoCard from "@/app/uploads/VideoCard"
+import UploadsSkeleton from './UploadsSkeleton'
 
 const BUCKET_NAME = process.env.NEXT_PUBLIC_BUCKET_NAME
 const BUCKET_REGION = process.env.NEXT_PUBLIC_BUCKET_REGION
 const thumbnailUrl = `https://${BUCKET_NAME}.s3.${BUCKET_REGION}.amazonaws.com/thumbnails`
 
-export default function UploadsPage() {
+export default function Uploads() {
 
     const searchParams = useSearchParams()
     const limit = parseInt(searchParams.get('limit')!) || 10
@@ -40,27 +41,22 @@ export default function UploadsPage() {
     }, [limit, offset, refresh])
 
     return (
-        <main className="p-2 bg-white">
-            <div className='flex justify-between mx-2 mb-4'>
-                <h1>My Uploads</h1>
+        <>
+            <div className='flex justify-between mx-2 my-4'>
+                <p>My Uploads</p>
                 <Button onClick={() => setRefresh((prevVal) => !prevVal)}>Refresh</Button>
-            </div>
-
+            </div>    
             <div className="flex">
-                {isLoading ? <p className="text-center">Loading...</p> : <>
+                { isLoading ? <UploadsSkeleton /> : 
                 <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6">
-                {/* <div className="w-full flex flex-wrap"> */}
                     {videos.map((video : Video & { name: string }) => (
                         <div key={video.id} className='mx-2 mb-6'>
-                          {/* <Link href={`/watch?v=${video.video_id}`}> */}
                           <VideoCard title={video.title} video_id={video.video_id} thumbnailUrl={`${thumbnailUrl}/${video.thumbnail_id}`} setRefresh={setRefresh}/>
-                          {/* </Link> */}
                         </div>
                     ))}
-                    </div>
-                </>}
+                </div>
+                }
             </div>
-
-        </main>
+        </>
     )
 }
