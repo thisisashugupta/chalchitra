3204f3ef0d2c50dabdc50bce467e3f4536ca5e72
Ashu Gupta <ashugpt1812@gmail.com>
2024-03-16 16:32:27 +0530

    moved video player to client component

--- src/app/watch/WatchPage.tsx
+++ src/app/watch/WatchPage.tsx
@@ -1,5 +1,6 @@
 'use client'
 import RecoilProvider from '@/app/providers/RecoilProvider'
+import VideoPlayer from '@/app/watch/VideoPlayer'
 
 interface WatchPageProps {
     children: React.ReactNode
@@ -9,6 +10,7 @@ export default function WatchPage({ children }: WatchPageProps) {
 
     return (
         <RecoilProvider>
+            <VideoPlayer/>
             {children}
         </RecoilProvider>
     )
--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -3,7 +3,6 @@
 import { Suspense } from 'react'
 import { permanentRedirect } from 'next/navigation'
 import WatchPage from '@/app/watch/WatchPage'
-import VideoPlayer from '@/app/watch/VideoPlayer'
 import VideoDetails from '@/app/watch/VideoDetails'
 import Suggestions from '@/app/watch/Suggestions'
 import { VideoDetailsSkeleton, SuggestionsSkeleton } from './loading'
@@ -13,22 +12,16 @@ interface WatchPageProps {
 }
 
 export default function Page({ searchParams }: WatchPageProps) {
-
     const v = searchParams.v;
     if (!v) permanentRedirect('/');
 
-    // const videoUrl = `https://${BUCKET_NAME}.s3.${BUCKET_REGION}.amazonaws.com/videos/${v}`
-
     return (
         <main className='flex justify-center'>
-            <div className='w-full max-w-[106.5rem] md:mx-6 flex flex-col lg:flex-row gap-6 items-start justify-center'>
+            <div className='w-full max-w-[106.5rem] md:mx-6 flex flex-col gap-6 items-start justify-center'>
             <WatchPage>
-                <div className='w-full'>
-                    <VideoPlayer/>
-                    <Suspense fallback={<VideoDetailsSkeleton/>}>
-                        <VideoDetails v={v as string} />
-                    </Suspense>
-                </div>
+                <Suspense fallback={<VideoDetailsSkeleton/>}>
+                    <VideoDetails v={v as string} />
+                </Suspense>
                 <Suspense fallback={<SuggestionsSkeleton/>}>
                     <Suggestions />
                 </Suspense>
