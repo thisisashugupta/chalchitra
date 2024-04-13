d88e81fa3b93629a3c44943d2414fe6e6bff6558
Ashu Gupta <ashugpt1812@gmail.com>
2024-04-13 20:12:07 +0530

    refactor: shifted recoil provider to layout

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -1,8 +1,5 @@
-// 'use server'
-
 import { Suspense } from 'react'
 import { permanentRedirect } from 'next/navigation'
-import WatchPage from '@/app/watch/WatchPage'
 import VideoPlayer from '@/app/watch/VideoPlayer' // client component
 import VideoDetails from '@/app/watch/VideoDetails' // server component
 import Suggestions from '@/app/watch/Suggestions' // server component
@@ -19,18 +16,16 @@ export default function Page({ searchParams }: WatchPageProps) {
     return (
         <main className='flex justify-center'>
             <div className='w-full max-w-[106.5rem] md:mx-6 flex flex-col lg:flex-row gap-6 items-start justify-center'>
-                <WatchPage>
-                    <div className='w-full'>
+                <div className='w-full'>
                     <VideoPlayer video_id={v as string}>
                     <Suspense fallback={<VideoDetailsSkeleton/>}>
                         <VideoDetails v={v as string} />
                     </Suspense>
                     </VideoPlayer>
-                    </div>
-                    <Suspense fallback={<SuggestionsSkeleton/>}>
-                        <Suggestions />
-                    </Suspense>
-            </WatchPage>
+                </div>
+                <Suspense fallback={<SuggestionsSkeleton/>}>
+                    <Suggestions />
+                </Suspense>
             </div>
         </main>
     )
