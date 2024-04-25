a254cfd11f89c480f0c4eecad80e2ac5a874bf16
Ashu Gupta <ashugpt1812@gmail.com>
2024-04-25 15:58:03 +0530

    chore: protected feed routed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export {default} from 'next-auth/middleware';
 
-export const config = { matcher: ["/upload", "/uploads", "/uploadViaServer"] }
+export const config = { matcher: ["/upload", "/uploads", "/uploadViaServer", "/feed/you", "/feed/subscriptions", "/feed/playlists", "/feed/downloads", "/feed/channels"] }
